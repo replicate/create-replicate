662c640b32a39aa0f3e6a592baf789ef93ffe1b4
Zeke Sikelianos <zeke@sikelianos.com>
2024-01-30 14:20:47 -0800

    use a node.js throwback prompt

--- template/index.js
+++ template/index.js
@@ -3,7 +3,7 @@ import Replicate from 'replicate'
 const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN })
 const model = 'stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b'
 const input = {
-  prompt: 'An astronaut riding a rainbow unicorn, cinematic, dramatic',
+  prompt: 'A retro comic book rendering of a turtle with a rocket strapped to its back.',
   num_outputs: 1
 }
 
