62055621a848ddfc802eab677abb33e5274443ca
Zeke Sikelianos <zeke@sikelianos.com>
2024-01-30 13:58:16 -0800

    use standard

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
 
-const targetAppName = process.argv[2] || 'my-replicate-app';
+const targetAppName = process.argv[2] || 'my-replicate-app'
 
-console.log(`Creating app "${targetAppName}"...`);
-console.log("Copying files...");
-const templateDir = path.join(__dirname, 'template');
-const targetDir = path.join(process.cwd(), targetAppName);
-fs.cpSync(templateDir, targetDir, { recursive: true });
+console.log(`Creating app "${targetAppName}"...`)
+console.log('Copying files...')
+const templateDir = path.join(__dirname, 'template')
+const targetDir = path.join(process.cwd(), targetAppName)
+fs.cpSync(templateDir, targetDir, { recursive: true })
 
+console.log('Setting package name...')
+execSync(`npm pkg set name=${targetAppName}`, { cwd: targetDir, stdio: 'inherit' })
 
-console.log("Setting package name...");
-execSync(`npm pkg set name=${targetAppName}`, { cwd: targetDir, stdio: 'inherit' });
+console.log('Installing dependencies...')
+execSync('npm install', { cwd: targetDir, stdio: 'inherit' })
 
-console.log("Installing dependencies...");
-execSync('npm install', { cwd: targetDir, stdio: 'inherit' });
-
-
-console.log("\n\nReplicate app created successfully!");
-console.log(`Try it out by running the following commands:\n\ncd ${targetAppName} && npm start`);
+console.log('\n\nReplicate app created successfully!')
+console.log(`Try it out by running the following commands:\n\ncd ${targetAppName} && npm start`)
--- template/index.js
+++ template/index.js
@@ -1,14 +1,14 @@
-import Replicate from "replicate";
+import Replicate from 'replicate'
 
-const replicate = new Replicate({auth: process.env.REPLICATE_API_TOKEN});
-const model = "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b"
+const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN })
+const model = 'stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b'
 const input = {
-  prompt: "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-  num_outputs: 1,
+  prompt: 'An astronaut riding a rainbow unicorn, cinematic, dramatic',
+  num_outputs: 1
 }
 
-console.log("Model:", model);
-console.log("Prompt:", input.prompt);
-console.log("Running...")
-const output = await replicate.run(model, {input});
-console.log("Done!", output);
+console.log('Model:', model)
+console.log('Prompt:', input.prompt)
+console.log('Running...')
+const output = await replicate.run(model, { input })
+console.log('Done!', output)
