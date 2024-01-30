785ea55cf3f5d318f056e6a1f2695de16ebb1da0
Zeke Sikelianos <zeke@sikelianos.com>
2024-01-30 13:47:28 -0800

    allow setting project name as a CLI arg

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
-const targetAppName = 'my-replicate-app';
+const targetAppName = process.argv[2] || 'my-replicate-app';
 
-console.log("Creating a Replicate app...");
+console.log(`Creating app "${targetAppName}"...`);
 console.log("Copying files...");
 const templateDir = path.join(__dirname, 'template');
 const targetDir = path.join(process.cwd(), targetAppName);
 fs.cpSync(templateDir, targetDir, { recursive: true });
 
-const { execSync } = require('child_process');
+
+console.log("Setting package name...");
+execSync(`npm pkg set name=${targetAppName}`, { cwd: targetDir, stdio: 'inherit' });
 
 console.log("Installing dependencies...");
 execSync('npm install', { cwd: targetDir, stdio: 'inherit' });
