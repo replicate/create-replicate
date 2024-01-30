6e8e2f4ed50bac98cb3b85fedef190ce595110ea
Zeke Sikelianos <zeke@sikelianos.com>
2024-01-30 13:55:38 -0800

    just one output at first so it's snappier

--- template/index.js
+++ template/index.js
@@ -4,7 +4,7 @@ const replicate = new Replicate({auth: process.env.REPLICATE_API_TOKEN});
 const model = "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b"
 const input = {
   prompt: "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-  num_outputs: 4,
+  num_outputs: 1,
 }
 
 console.log("Model:", model);
