218e1c53280d30a2f36c78595d36a49eb7281c40
Zeke Sikelianos <zeke@sikelianos.com>
2024-02-07 00:19:27 -0800

    remove superfluous error message

--- template/index.js
+++ template/index.js
@@ -2,14 +2,6 @@ import Replicate from 'replicate'
 import dotenv from 'dotenv'
 dotenv.config()
 
-if (!process.env.REPLICATE_API_TOKEN) {
-  console.error('REPLICATE_API_TOKEN not found in environment')
-  console.error('To copy a token, go to https://replicate.com/account')
-  console.error('Then add your token to a local .env file like this:')
-  console.error('echo "REPLICATE_API_TOKEN=your-token" > .env')
-  process.exit()
-}
-
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
   userAgent: 'https://www.npmjs.com/package/create-replicate'
