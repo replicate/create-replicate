9160524f7dd89394dc26649cd728810d64254a46
Zeke Sikelianos <zeke@sikelianos.com>
2024-02-07 00:29:09 -0800

    use public Replicate API to get model metadata
    and test it. getting that mocking setup right was the hard part 😅

--- index.test.js
+++ index.test.js
@@ -23,8 +23,9 @@ describe('Node script test', () => {
   })
 
   it('should create a directory with expected files', () => {
-    const command = `REPLICATE_API_TOKEN=test_token node index.mjs ${directoryName} --run-after-setup=false`
+    const command = `REPLICATE_API_TOKEN=r8_test_token node index.mjs ${directoryName} --run-after-setup=false`
 
+    // set stdio to 'inherit' to see script output in test output
     execSync(command, { stdio: 'ignore', env: process.env })
 
     // Check if the directory exists
@@ -40,7 +41,7 @@ describe('Node script test', () => {
     const envFile = path.join(directoryName, '.env')
     expect(fileExists(envFile)).toBe(true)
     const envFileContents = fs.readFileSync(envFile, 'utf8')
-    expect(envFileContents).toBe('REPLICATE_API_TOKEN=test_token')
+    expect(envFileContents).toBe('REPLICATE_API_TOKEN=r8_test_token')
 
     // Check if .gitignore exists in the directory
     const gitignoreFile = path.join(directoryName, '.gitignore')
@@ -50,8 +51,9 @@ describe('Node script test', () => {
   })
 
   it('handles basic `model` argument in the form {owner}/{model}', () => {
-    const command = `REPLICATE_API_TOKEN=test_token node index.mjs ${directoryName} --model=yorickvp/llava-13b --run-after-setup=false`
+    const command = `REPLICATE_API_TOKEN=r8_test_token node index.mjs ${directoryName} --model=yorickvp/llava-13b --run-after-setup=false`
 
+    // set stdio to 'inherit' to see script output in test output
     execSync(command, { stdio: 'ignore', env: process.env })
 
     // Check if the directory exists
@@ -63,12 +65,13 @@ describe('Node script test', () => {
 
     // Check if index.js contains the correct model name
     const indexFileContents = fs.readFileSync(indexFile, 'utf8')
-    expect(indexFileContents).toMatch(/yorickvp\/llava-13b:[a-zA-Z0-9]{40}/)
+    expect(indexFileContents).toMatch(/yorickvp\/llava-13b:[a-zA-Z0-9]{64}/)
   })
 
   it('handles a `model` argument in the form {owner}/{model}:{version}', () => {
-    const command = `REPLICATE_API_TOKEN=test_token node index.mjs ${directoryName} --model=yorickvp/llava-13b:2cfef05a8e8e648f6e92ddb53fa21a81c04ab2c4f1390a6528cc4e331d608df8 --run-after-setup=false`
+    const command = `REPLICATE_API_TOKEN=r8_test_token node index.mjs ${directoryName} --model=yorickvp/llava-13b:2cfef05a8e8e648f6e92ddb53fa21a81c04ab2c4f1390a6528cc4e331d608df8 --run-after-setup=false`
 
+    // set stdio to 'inherit' to see script output in test output
     execSync(command, { stdio: 'ignore', env: process.env })
 
     // Check if the directory exists
